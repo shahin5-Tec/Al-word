4e20da7697d6884a5e1f2b5004122409ba81fadc
Shahin Hossain <shahiniubat99@gmail.com>
2023-07-23 00:42:28 +0600

    Header-part

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='mt-3'>
+            <h2 className='text-3xl font-bold text-center'>AI Universe Hub</h2>
+        </div>
+    );
+};
+
+export default Header;
